9e5b3f51c6e0c576e22633ebf008b0f0dea698ff
Alex Reznichenko <skywoker94@gmail.com>
2015-05-26 23:18:38 +0300

    Added window "fmStructCatalog"

--- NTFSStruct/NTFSStruct/fmStructCatalog.xaml.cs
+++ NTFSStruct/NTFSStruct/fmStructCatalog.xaml.cs
@@ -23,5 +23,61 @@ namespace NTFSStruct
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\1.png", imSmallCatalog);
+            Helper.LoadImage(@"image\CatalogStruct\3.png", imBigCatalog);
+            Helper.LoadImage(@"image\CatalogStruct\6.png", imVirt);
+            Helper.LoadImage(@"image\void.png", imIndexBuf);
+            
+        }
+
+        private void imSmallCatalog_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\2.png", imSmallCatalog);
+        }
+
+        private void imSmallCatalog_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\1.png", imSmallCatalog);
+        }
+
+        private void imBigCatalog_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\4.png", imBigCatalog);
+        }
+
+        private void imBigCatalog_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\3.png", imBigCatalog);
+        }
+
+        private void imIndexBuf_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\5.png", imBigCatalog);
+        }
+
+        private void imIndexBuf_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\3.png", imBigCatalog);
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\7.png", imVirt);
+        }
+
+        private void imVirt_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Helper.LoadImage(@"image\CatalogStruct\6.png", imVirt);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            
+            fmStructNTFS form = new fmStructNTFS();
+            form.Show();
+        }
     }
 }
